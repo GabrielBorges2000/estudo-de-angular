be2e220619273a797cf6debb14190d80ede464bc
GabrielBorges2000 <gabriel.vscode@gmail.com>
2024-02-27 00:12:15 -0300

    Initial Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>
+      Hello World!
+    </h1>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
